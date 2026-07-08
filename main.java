0d9ef0b64d060c4cb959c74470dace582ed81301
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-08 08:32:35 +0800

    Setting up main class and method

--- main.java
+++ main.java
@@ -1,3 +1,8 @@
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+
+public class PomodoroTimer{
+  private static Timer timer = new Timer;
+  private static TimeLeft;
+}
