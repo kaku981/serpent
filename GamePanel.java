3be44ad186099dd1092656fe7544f07327adbcf3
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-20 09:06:45 +0800

    Added move function and a snake

--- GamePanel.java
+++ GamePanel.java
@@ -3,8 +3,6 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-import javax.swing.JPanel;
-
 public class GamePanel extends JPanel implements ActionListener{
     static final int SCREEN_WIDTH = 500;
     static final int SCREEN_HEIGHT = 500;
@@ -47,12 +45,43 @@ public class GamePanel extends JPanel implements ActionListener{
         }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for(int i = 0; i < bodyParts; i++){
+            if(i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+
+        }
     }
     public void newApple(){
         appleX = random.nextInt((SCREEN_WIDTH/UNIT_SIZE)) * UNIT_SIZE;
         appleY = random.nextInt((SCREEN_HEIGHT/UNIT_SIZE) * UNIT_SIZE);
     }
     public void move(){
+        for(int i = bodyParts; i > 0 ; i--) {
+                x[i] = x[i-1];
+                y[i] = y[i-1];
+        }
+
+        switch(direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
 
     }
     public void checkApple(){
